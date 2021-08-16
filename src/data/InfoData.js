484c8629eb3b2fba19eaf028ea614173ea6500bf
doollawat <85749542+doollawat@users.noreply.github.com>
2021-08-16 16:19:34 +0700

    Add files via upload

---
+++ src/data/InfoData.js
@@ -0,0 +1,23 @@
+import ImageOne from '../images/interior-1.jpg';
+import ImageTwo from '../images/house-5.jpg';
+
+export const InfoData = {
+  heading: 'Explore our beautiful house',
+  paragraphOne: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. ',
+  paragraphTwo:
+    'Cras at mi eu enim vehicula volutpat. Aenean vel mattis erat. Morbi suscipit mauris vitae neque ullamcorper, id ultrices odio bibendum. Donec eleifend ac magna eget pharetra.',
+  buttonLabel: 'View Homes',
+  image: ImageOne,
+  reverse: false,
+  delay: 100,
+};
+
+export const InfoDataTwo = {
+  heading: 'Modern House',
+  paragraphOne: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. ',
+  paragraphTwo: 'Cras at mi eu enim vehicula volutpat. Aenean vel mattis erat.',
+  buttonLabel: 'View Homes',
+  image: ImageTwo,
+  reverse: true,
+  delay: 300,
+};
---
+++ src/data/SliderData.js
@@ -0,0 +1,69 @@
+import ImageOne from '../images/house-1.jpg';
+import ImageTwo from '../images/house-2.jpg';
+import ImageThree from '../images/house-3.jpg';
+import ImageFour from '../images/house-4.jpg';
+// import ImageFive from '../images/house-5.jpg';
+// import ImageSix from '../images/house-6.jpg';
+// import ImageSeven from '../images/house-7.jpg';
+
+export const SliderData = [
+  {
+    title: 'Luxury Villa in Bangkok, Thailand',
+    price: '$4,000,000',
+    path: '/homes',
+    label: 'View Home',
+    image: ImageOne,
+    alt: 'House',
+  },
+  {
+    title: 'House in Krabi, Thailand',
+    price: '$4,400,000',
+    path: '/homes',
+    label: 'View Home',
+    image: ImageTwo,
+    alt: 'House',
+  },
+  {
+    title: 'House in Cha-am, Thailand',
+    price: '$7,900,000',
+    path: '/homes',
+    label: 'View Home',
+    image: ImageThree,
+    alt: 'House',
+  },
+  {
+    title: 'House in Hua-hin, Thailand',
+    price: '$5,880,000',
+    path: '/homes',
+    label: 'View Home',
+    image: ImageFour,
+    alt: 'House',
+  },
+];
+
+// export const SliderDataTwo = [
+//   {
+//     title: 'House in Bangkok, Thailand',
+//     price: '$9,000,000',
+//     path: '/homes',
+//     label: 'View Home',
+//     image: ImageFive,
+//     alt: 'House',
+//   },
+//   {
+//     title: 'House in Lamphun, Thailand',
+//     price: '$4,000,000',
+//     path: '/homes',
+//     label: 'View Home',
+//     image: ImageSix,
+//     alt: 'House',
+//   },
+//   {
+//     title: 'House in Chiangmai, Thailand',
+//     price: '$7,998,000',
+//     path: '/homes',
+//     label: 'View Home',
+//     image: ImageSeven,
+//     alt: 'House',
+//   },
+// ];
